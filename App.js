0dc92614c9311a1bddac61d92e601310e5db389b
AyonGupta <ayon.gupta@wwindia.com>
2021-01-13 00:14:37 +0530

    Working on Map

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+* Sample React Native App
+* https://github.com/facebook/react-native
+*
+* @format
+* @flow strict-local
+*/
 import 'react-native-gesture-handler';
 import React from 'react';
 import SplashPage from './View/SplashPage'
@@ -12,31 +12,39 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginPage from './View/LoginPage'
 import Dashboard from './View/Dashboard'
+import AboutUs from "./View/AboutUs";
 
 const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
     <Stack.Navigator initialRouteName='Splash'>
-      <Stack.Screen name='Splash'
-                    component={SplashPage}
-                    options = {{
-                      headerShown : false                
-                    }}/>
-      <Stack.Screen name='Login' 
-                    component={LoginPage}
-                    options = {{
-                      headerShown : false
-                    }}/>
+    <Stack.Screen name='Splash'
+    component={SplashPage}
+    options = {{
+      headerShown : false                
+    }}/>
+    <Stack.Screen name='Login' 
+    component={LoginPage}
+    options = {{
+      headerShown : false
+    }}/>
+    
+    <Stack.Screen name='Dashboard'
+    component={Dashboard}
+    options = {{
+      headerShown : false
+    }}/>
+    <Stack.Screen name='AboutUs'
+    component={AboutUs}
+    options = {{
+      headerShown : true
+    }}/>
 
-      <Stack.Screen name='Dashboard'
-                    component={Dashboard}
-                    options = {{
-                      headerShown : false
-                    }}/>
     </Stack.Navigator>
     </NavigationContainer>
-);
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
+  
---
+++ View/AboutUs.js
@@ -0,0 +1,30 @@
+import { React, useState, useEffect } from "react";
+import { 
+    View,
+    Text,
+    SafeAreaView
+} from "react-native";
+import MapView from 'react-native-maps';
+
+const AboutUs =  () => {
+
+    const [latitude, setLatitude] = useState (null);
+    const [longitude, setLongitude] = useState (null);
+
+    useEffect (()=> {
+
+
+    })
+    return (
+        <SafeAreaView>
+        <View>
+        
+        </View>
+        </SafeAreaView>
+        
+        );
+        
+        
+    }
+
+    export default AboutUs;
